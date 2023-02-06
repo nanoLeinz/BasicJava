7638612d526a6560909e812a8606e1a2cfb01c9f
Awal Kautsar <Awallkautsar@gmail.com>
2023-02-06 13:45:52 +0700

    until method

--- src/Dasar.java
+++ src/Dasar.java
@@ -2,15 +2,30 @@ public class Dasar {
     public static void main(String[] args) {
         sayMyName("nano", "leinz");
         sayMyName("niko", "leinz");
-        System.out.println(div(10,2));
+        System.out.println(div(10, 2));
+        average("nano",80,30,90,90,10,10,1000);
     }
 
-    static void sayMyName(String first, String last){
-        System.out.println("I'm "+first+" "+last);
+    static void sayMyName(String first, String last) {
+        System.out.println("I'm " + first + " " + last);
     }
 
-    static Integer div(Integer val1,Integer val2){
+    static Integer div(Integer val1, Integer val2) {
         return val1 / val2;
     }
+
+    static void average(String name, Integer... nilai) {
+        Integer total = 0;
+        for (Integer val : nilai
+        ) {
+            total += val;
+        }
+        total /= nilai.length;
+        System.out.println(total >= 70 ?
+                "Selamat " + name + ", anda lulus dengan nilai : " + total :
+                "Sayang sekali " + name + ", anda tidak lulus karena nilai anda : " + total);
+    }
+
+
 
 }
