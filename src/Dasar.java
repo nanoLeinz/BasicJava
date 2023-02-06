36d28f2016ac1c1cca6e0410115d495e1cbd606c
Awal Kautsar <Awallkautsar@gmail.com>
2023-02-06 13:22:07 +0700

    until method

--- src/Dasar.java
+++ src/Dasar.java
@@ -1,3 +1,16 @@
 public class Dasar {
+    public static void main(String[] args) {
+        sayMyName("nano", "leinz");
+        sayMyName("niko", "leinz");
+        System.out.println(div(10,2));
+    }
+
+    static void sayMyName(String first, String last){
+        System.out.println("I'm "+first+" "+last);
+    }
+
+    static Integer div(Integer val1,Integer val2){
+        return val1 / val2;
+    }
 
 }
